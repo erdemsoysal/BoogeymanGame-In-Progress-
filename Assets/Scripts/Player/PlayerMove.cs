e6def9827bc2e9b400eaa3466850512f39c8a589
erdemsoysal <kutsal.erdem.soysal@gmail.com>
2021-12-11 20:24:00 +0300

    FirstCommit

---
+++ Assets/Scripts/Player/PlayerMove.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+    CharacterController playerController;
+    public float speed = 3;
+    public float gravity = 9.8f;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerController = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        PlayerMovement();
+
+
+    }
+    
+    
+    Vector3 moveVector;
+    private void PlayerMovement()
+    {
+        moveVector = new Vector3(Input.GetAxis("Horizontal") * speed * Time.deltaTime, 0, Input.GetAxis("Vertical") * speed * Time.deltaTime);
+        moveVector = transform.TransformDirection(moveVector);
+        gravityEffect();
+        playerController.Move(moveVector);
+
+
+
+    }
+   
+    #region Gravity
+    void gravityEffect()
+    {
+        if (!playerController.isGrounded)
+        {
+            moveVector.y -= Time.deltaTime * gravity;
+        }
+    }
+    #endregion
+
+
+
+
+
+
+
+
+}
---
+++ Assets/Scripts/Player/PlayerRotate.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerRotate : MonoBehaviour
+{
+
+    public float sensitivity = 3;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rotatePlayer();
+    }
+
+
+    private float rotY;
+    private float rotX;
+    private void rotatePlayer()
+    {
+        rotY += sensitivity * Input.GetAxis("Mouse Y")*-1;
+        rotX += sensitivity * Input.GetAxis("Mouse X");
+        rotY = Mathf.Clamp(rotY, -80, 80);
+        transform.eulerAngles = new Vector3(rotY, rotX,0);
+    }
+}
