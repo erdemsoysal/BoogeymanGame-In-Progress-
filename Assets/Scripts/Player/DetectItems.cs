6fbfb288992f55a81818b3661ef1b3b2d310a864
erdemsoysal <kutsal.erdem.soysal@gmail.com>
2021-12-12 08:00:56 +0300

    Update #3
    Kapı prefab, kapı anim, kapı aç/kapa, ışıklandırma, ışık aç/kapa

--- Assets/Scripts/Player/DetectItems.cs
+++ Assets/Scripts/Player/DetectItems.cs
@@ -8,35 +8,68 @@ public class DetectItems : MonoBehaviour
 {
     public Camera camera;
     public Image crosshair;
-    public GameObject light;
+    [Header("DOOR")]
+    public Animator doorAnim;
+    [Header("LIGHTS")]
+    public Light bedSideLight;
+    public Light Room1Light;
 
     // Start is called before the first frame update
     void Start()
     {
-        light = GetComponentInChildren<GameObject>();
+        bedSideLight.enabled = false;
+        Room1Light.enabled = false;
     }
 
     // Update is called once per frame
     void Update()
     {
         crosshair.color = Color.white;
-        light.SetActive(false);
         DetectItemScript();
     }
 
     private void DetectItemScript()
     {
         RaycastHit hit;
-        if (Physics.Raycast(camera.transform.position,camera.transform.forward,out hit,10))
+        if (Physics.Raycast(camera.transform.position,camera.transform.forward,out hit,2))
         {
+            // [LIGHT SECTION]
+            #region bedSideLight
             if (hit.collider.gameObject.tag == "lightSwitch")
             {
                 crosshair.color = Color.red;
-                if (Input.GetKey(KeyCode.F))
+                if (Input.GetKeyDown(KeyCode.F))
                 {
-                    light.SetActive(true);
+                    bedSideLight.enabled = !bedSideLight.enabled;
                 }
             }
+            #endregion
+            #region Room1Light
+            if (hit.collider.gameObject.tag == "lightSwitchRoom1")
+            {
+                crosshair.color = Color.red;
+                if (Input.GetKeyDown(KeyCode.F))
+                {
+                    Room1Light.enabled = !Room1Light.enabled;
+                }
+            }
+            #endregion
+
+            // [DOOR]
+
+            #region Door
+            if (hit.collider.gameObject.tag == "Door")
+            {
+                crosshair.color = Color.red;
+                doorAnim = hit.collider.GetComponentInChildren<Animator>();
+                if (Input.GetKeyDown(KeyCode.F))
+                {
+                    doorAnim.SetTrigger("OpenClose");
+                }
+            }
+            #endregion
+
+
         }
     }
 }
