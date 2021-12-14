c46c3ba3895906ddef0de98e888dec50b03656c2
erdemsoysal <kutsal.erdem.soysal@gmail.com>
2021-12-15 01:23:26 +0300

    Update #4
    Yatağa yatma, ışıklandırma(devam ediyor)

--- Assets/Scripts/Player/DetectItems.cs
+++ Assets/Scripts/Player/DetectItems.cs
@@ -10,15 +10,31 @@ public class DetectItems : MonoBehaviour
     public Image crosshair;
     [Header("DOOR")]
     public Animator doorAnim;
+    [Header("SLEEP")]
+    public GameObject playerCam;
+    public bool isSleeping;
     [Header("LIGHTS")]
     public Light bedSideLight;
     public Light Room1Light;
+    public Light HallwayLight;
+    public Light CorridorLight;
+    public Light Room2Light;
 
+
+    PlayerMove playerMove;
     // Start is called before the first frame update
     void Start()
     {
+        #region Lights
         bedSideLight.enabled = false;
         Room1Light.enabled = false;
+        //HallwayLight.enabled = false;
+        CorridorLight.enabled = false;
+        Room2Light.enabled = false;
+
+        #endregion
+
+        isSleeping = false;
     }
 
     // Update is called once per frame
@@ -54,6 +70,36 @@ public class DetectItems : MonoBehaviour
                 }
             }
             #endregion
+            #region HallwayLight
+            if (hit.collider.gameObject.tag == "lightSwitchHallway")
+            {
+                crosshair.color = Color.red;
+                if (Input.GetKeyDown(KeyCode.F))
+                {
+                    HallwayLight.enabled = !HallwayLight.enabled;
+                }
+            }
+            #endregion
+            #region CorridorLight
+            if (hit.collider.gameObject.tag == "lightSwitchCorridor")
+            {
+                crosshair.color = Color.red;
+                if (Input.GetKeyDown(KeyCode.F))
+                {
+                    CorridorLight.enabled = !CorridorLight.enabled;
+                }
+            }
+            #endregion
+            #region Room2Light
+            if (hit.collider.gameObject.tag == "lightSwitchRoom2")
+            {
+                crosshair.color = Color.red;
+                if (Input.GetKeyDown(KeyCode.F))
+                {
+                    Room2Light.enabled = !Room2Light.enabled;
+                }
+            }
+            #endregion
 
             // [DOOR]
 
@@ -69,6 +115,34 @@ public class DetectItems : MonoBehaviour
             }
             #endregion
 
+            // [LAYING ON BED]
+            #region Laying On Bed [IN PROGRESS]
+
+            if (hit.collider.gameObject.tag == "Bed")
+            {
+                crosshair.color = Color.red;
+                playerMove = GetComponent<PlayerMove>();
+                if (Input.GetKeyDown(KeyCode.F) && !isSleeping)
+                {
+                    playerCam.transform.position = new Vector3(-3.153f, 0.748f, -0.943f);
+                    playerCam.transform.rotation = Quaternion.Euler(0,0,90);
+                    isSleeping = true;
+                    if (isSleeping)
+                    {
+                        playerMove.speed = 0;
+                    }
+
+                }
+                else if (Input.GetKeyDown(KeyCode.F) && isSleeping)
+                {
+                    playerMove.speed = 3;
+                    playerCam.transform.localPosition = new Vector3(0, 0.656f, 0);
+                    isSleeping = false;
+                }
+            }
+            
+
+            #endregion
 
         }
     }
